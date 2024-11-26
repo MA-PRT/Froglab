7a32d80604b55385111ccaa3d4d0c332d1c77841
Matthieu Parent <matthieu.parent78@gmail.com>
2024-11-26 14:21:12 +0100

    relations improved

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,10 @@
 class Team < ApplicationRecord
   belongs_to :parent_team, class_name: "Team", optional: true
+
+  has_many :child_teams, class_name: 'Team', foreign_key: 'parent_team_id'
+  has_many :team_memberships, class_name: 'TeamMember'
+  has_many :users, through: :team_memberships
+  has_many :team_communications
+  has_many :communications, through: :team_communications
+  has_many :tickets
 end
