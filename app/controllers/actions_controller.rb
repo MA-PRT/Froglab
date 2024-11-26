65a508cc1592224193bf74f45bcad37a970a5ce4
Matthieu Parent <matthieu.parent78@gmail.com>
2024-11-26 17:33:05 +0100

    controllers in progress

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,5 +1,6 @@
 class ActionsController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :set_rocket, only: %i[ show edit update destroy ]
   def index
   end
 
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,20 +1,40 @@
 class TicketsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_ticket, only: %i[ show destroy ]
 
   def index
+    @tickets = Ticket.all
   end
 
   def show
   end
 
   def new
+    @ticket = Ticket.new
   end
 
   def create
+    @ticket = Ticket.new(params_ticket)
+    @ticket.user = current_user
+    if @ticket.save
+      redirect_to ticket_path(@ticket)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @ticket.destroy
+    redirect_to tickets_path
   end
 
   private
 
+  def params_ticket
+    params.require(:ticket).permit(:title, :content, :priority, , photos: [])
+  end
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
 end
