bf378dfe150ba5f2e87707b4752d41f8c4f68840
Matthieu Parent <matthieu.parent78@gmail.com>
2024-11-26 15:28:16 +0100

    Controllers DONE + Routes DONE

---
+++ app/controllers/communications_controller.rb
@@ -0,0 +1,6 @@
+class CommunicationsController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  authenticated :user do
+    root to: "communications#index", as: :user_root
+  end
+
+  unauthenticated :user do
+    devise_scope :user do
+      root to: "devise/sessions#new"
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :communications, only: [ :index, :show, :new, :create, :destroy ]
+
+  resources :tickets do
+    resources :actions, only: [ :create ]
+  end
+
+  resources :actions, only: [ :index, :edit, :update, :destroy ]
+
 end
