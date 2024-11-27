777058eaf14459fc543ac4ecc01b98ae3e563dfc
Nicolas Garric <nicolasdfgarric@gmail.com>
2024-11-27 16:42:58 +0100

    Show ticket created. Almost done. First front ok. Colord for the progress bar and the status created and added to helpers/application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def progress_bar(state)
+    case state
+    when "very_low"
+      { number: "10", color:  "#1F4529" }
+    when "low"
+      { number: "25", color:  "#6EC207" }
+    when "medium"
+      { number: "50", color:  "#FA812F" }
+    when "high"
+      { number: "75", color: "#C40C0C" }
+    when "very_high"
+      { number: "100", color: "#FF0000" }
+    end
+  end
 end
