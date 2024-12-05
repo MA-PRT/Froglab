cffc677f9851bbb5be0a95ea181902f1236dcc39
Matthieu Parent <matthieu.parent78@gmail.com>
2024-12-05 15:50:23 +0100

    meta data OK essai 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  DOMAIN = "www.froglab.tech"
+
   def after_sign_in_path_for(resource)
     home_path
   end
+
+  def default_url_options
+    { host: ENV[DOMAIN] || "localhost:3000" }
+    end
 
 end
